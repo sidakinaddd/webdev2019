df986d6a3821e22adf6120a55144635bb9a403d4
Your Name <sidakinaddd@gmail.com>
2019-04-02 12:02:02 +0500

    lection's

--- realweek10/Project2/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\user\desktop\webdev2019\realweek10\project2\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
